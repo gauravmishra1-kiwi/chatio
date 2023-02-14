d66aa487a1a625ffe7fe7df53d83254a3714b7d6
gauravmishra1-kiwi <gaurav.mishra1@kiwitech.com>
2023-02-14 17:32:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const path =require('path')
+const http = require('http')
+const express=require('express')
+const app=express();
+const socketio=require('socket.io')
+
+const server=http.createServer(app)
+const io=socketio(server)
+
+const port=process.env.PORT||3000
+const publicDirectoryPath=path.join(__dirname,'./public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection',()=>{
+    console.log('new websocket connection');
+})
+
+
+server.listen(port,()=>{
+    console.log(`server at ${port}`);
+})
